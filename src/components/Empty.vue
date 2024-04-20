c37230cf255b4d5b3d0806205e03c41bbfde9848
fernando3312 <ffzunzunegui98@gmail.com>
2024-04-20 16:07:52 +0200

    mejor del componente empty

--- src/components/Empty.vue
+++ src/components/Empty.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <p>No existen tareas registradas.</p>
+    <p>No existen tareas registradas</p>
+    <p v-if="msg">En la seccion:{{ msg }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Empty",
+  props: {
+    msg: {
+      required: false,
+      type: String,
+    },
+  },
+};
+</script>
 
 <style></style>
