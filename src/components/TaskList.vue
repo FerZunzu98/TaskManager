131d579da89b946eba61455ea4c792f28ed232d5
fernando3312 <ffzunzunegui98@gmail.com>
2024-04-20 16:09:48 +0200

    Division tareas completadas y no

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
     <div v-if="!isEmpty">
-      {{ tasks }}
-      <Task v-for="task of tasks" :key="task.id" />
+      <div>
+        <h2>Tarenas no completadas</h2>
+        <div v-if="!isIncompletedEmpty">
+          <Task v-for="task of incompleted" :key="task.id" :task="task" />
+        </div>
+        <div v-else>
+          <Empty msg="Incompletas" />
+        </div>
+      </div>
+      <div>
+        <h2>Tareas completadas</h2>
+        <div v-if="!isCompletedEmpty">
+          <Task v-for="task of completed" :key="task.id" :task="task" />
+        </div>
+        <div v-else>
+          <Empty msg="Completadas" />
+        </div>
+      </div>
     </div>
     <div v-else>
       <Empty />
@@ -11,9 +27,10 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapGetters, mapState } from "vuex";
 import Empty from "@/components/Empty.vue";
 import Task from "@/components/Task.vue";
+import api from "@/services/api";
 
 export default {
   name: "TaskList",
@@ -23,9 +40,19 @@ export default {
   },
   computed: {
     ...mapState(["tasks"]),
+    ...mapGetters(["completed", "incompleted"]),
     isEmpty() {
       return this.tasks.length === 0;
     },
+    isCompletedEmpty() {
+      return this.completed.length === 0;
+    },
+    isIncompletedEmpty() {
+      return this.incompleted.length === 0;
+    },
+  },
+  mounted() {
+    api.getTasks().then((tasks) => console.log(tasks));
   },
 };
 </script>
